72b110e1099576a2f3714231e03b081684cca55e
Philipp <ph2003@gmx.net>
2023-07-02 14:08:49 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Philipp Sanktjohanser on 11.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
